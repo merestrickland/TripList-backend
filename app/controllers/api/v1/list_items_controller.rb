da9f9e5c75acbfdbfe190e89344641e885dd22bf
merestrickland <mere.strickland@gmail.com>
2019-05-30 10:15:13 -0400

    list items

--- app/controllers/api/v1/list_items_controller.rb
+++ app/controllers/api/v1/list_items_controller.rb
@@ -16,19 +16,31 @@ class Api::V1::ListItemsController < ApplicationController
         render json: @list_item, status: :accepted
     end
 
+    def show
+        @list_item = ListItem.find(params[:id])
+        render json: @list_item 
+    end
+
     def update 
         @list_item.update(list_item_params)
         if @list_item.save
-            render json: @list_item: :accepted 
+            render json: @list_item, status: :accepted 
         else
-            render json: { errors: @task.errors.full_messages }, status: :unprocessible entity
+            render json: { errors: @task.errors.full_messages }, status: :unprocessible_entity
         end 
     end 
+
+    def destroy 
+        @list_item = ListItem.find(params[:id])
+        @list_item.destroy
+        render json: {messages: 'this worked'}
+    end
+
 
     private
 
     def list_item_params
-        params.permit(list_item_id, :name, :description, :img_url)
+        params.permit(:id, :name, :description, :img_url, :user_id, :trip_id)
     end
 
     def find_list_item 
